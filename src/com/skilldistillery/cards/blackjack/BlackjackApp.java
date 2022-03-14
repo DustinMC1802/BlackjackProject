096bf15e148673cf18b100fae06f26d4f8d1ddde
DustinMC1802 <dustinmcollier@gmail.com>
2022-03-13 23:12:14 -0700

    Corrected the boolean methods in BlackjackApp

--- src/com/skilldistillery/cards/blackjack/BlackjackApp.java
+++ src/com/skilldistillery/cards/blackjack/BlackjackApp.java
@@ -9,68 +9,72 @@ public class BlackjackApp {
 	private Dealer dealer = new Dealer();
 	private Player player = new Player();
 	private Deck deck = new Deck();
-	
-	private boolean check = false;
-	
+
+
 	public static void main(String[] args) {
 		BlackjackApp app = new BlackjackApp();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		app.shuffleDeck();
 		app.dealInitialHands();
 		app.checkPlayerForBlackJack();
 		app.checkDealerForBlackJack();
-		app.checkForBust();
-		
+		app.checkPlayerForBust();
+		app.checkDealerForBust();
+		app.hitOrStand(sc);
 
 		sc.close();
 	}
-	
+
 	private void shuffleDeck() {
 		dealer.shuffle(deck);
 	}
-	
+
 	private void dealInitialHands() {
 		player.dealCard(deck.dealCard());
 		dealer.initialDeal(deck.dealCard());
 		player.dealCard(deck.dealCard());
 		dealer.dealCard(deck.dealCard());
 	}
-	
+
 	private void checkPlayerForBlackJack() {
-		check = player.checkForBlackjack();
-		if (check = true) {
+		if (player.checkForBlackjack()) {
 			System.out.println("You got a Blackjack!!");
 			System.out.println("Let's see what the Dealer gets...");
-			
+
 		} else {
-			check = false;
 		}
-		
-	} 
+
+	}
+
 	private void checkDealerForBlackJack() {
-		check = dealer.checkForBlackjack();
-		if (check = true) {
+		if (dealer.checkForBlackjack()) {
 			System.out.println("The Dealer got a Blackjack... Better luck next time");
 		} else {
-			check = false;
 		}
-		
-	} 
-	
-	private void checkForBust() {
-		check = player.checkForBust();
-		if (check = true) {
+
+	}
+
+	private void checkPlayerForBust() {
+		if (player.checkForBust()) {
 			System.out.println("You busted! Better luck next time ");
 		}
 	}
-	String answer = player.hitOrStand(sc, deck.dealCard());
-	
-	
-	while (answer.equals("H")) {
-		player.dealCard(deck.dealCard());
-		answer = player.hitOrStand(sc, deck.dealCard());
+
+	private void checkDealerForBust() {
+		if (dealer.checkForBlackjack()) {
+			System.out.println("The Dealer busted!! You win!!");
+		}
 	}
 
+	private void hitOrStand(Scanner sc) {
+		String answer = player.hitOrStand(sc, deck.dealCard());
+
+		while (answer.equals("H")) {
+			player.dealCard(deck.dealCard());
+			answer = player.hitOrStand(sc, deck.dealCard());
+		}
+
+	}
 }
